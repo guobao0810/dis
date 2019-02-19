57ed94da12e49c9b3c785372d37bbc589d8cc499
maomh <maomh1014@163.com>
2019-02-19 13:42:48 +0800

    修正申请记录明细的实体类

--- src/main/java/com/baoying/dis/entity/ApplyRecord.java
+++ src/main/java/com/baoying/dis/entity/ApplyRecord.java
@@ -3,11 +3,9 @@ package com.baoying.dis.entity;
 import com.baoying.dis.assets.base.BaseEntity;
 import lombok.Getter;
 import lombok.Setter;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnBean;
 
-import javax.persistence.Entity;
-import javax.persistence.JoinColumn;
-import javax.persistence.ManyToOne;
-import javax.persistence.Table;
+import javax.persistence.*;
 
 /**
  * 申请明细记录
@@ -16,24 +14,36 @@ import javax.persistence.Table;
 @Table(name = "t_apply_record")
 @Setter @Getter
 public class ApplyRecord extends BaseEntity {
-    // 月份
+    // 月份 1928-09
+    @Column(length = 15)
     private String month;
     // ID （客户 ID）
+    @Column(length = 20)
     private String acountId;
     // 序号 （证明文件上后缀的序号）
+    @Column(length = 10)
     private String seq;
     // 日期 （此证明日期）
+    @Column(length = 20)
     private String applyDate;
     // 商号名称 （经销商 公司名称）
+    @Column(length = 50)
     private String compName;
     // 金额
+    @Column(length = 20)
     private String money;
     // 中文大写的金额
+    @Column(length = 50)
     private String zhcnMoney;
     // 邮箱
+    @Column(length = 50)
     private String email;
     // 名称 （此证明开具人名）
+    @Column(length = 30)
     private String realName;
+    // 状态
+    @Column
+    private int status;
 
     @ManyToOne
     @JoinColumn(name="batch_no", referencedColumnName = "batch_no")
